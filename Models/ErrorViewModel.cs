7983682fce10db89e7d2ef99fe0c96a90db07a20
Davi Lopes Omena <davilomena@gmail.com>
2026-06-10 01:17:48 +0000

    Criação da base MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_MVC_Atividade_Uninove.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
